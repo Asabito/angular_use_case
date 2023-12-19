f38b19f3b859944b42f70525c79c56a03d01742f
Adie Laptop <a.satnir@gmail.com>
2023-12-19 16:38:22 +0700

    add table in home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  friendslist = [
+    {
+      name: 'Nishant',
+      negara: "Indonesia",
+      kota: "Jakarta",
+      balance: 900000
+    },
+    {
+      name: 'Shailesh',
+      negara: "USA",
+      kota: "Los Angeless",
+      balance: 590000
+    },
+    {
+      name: 'Akshay',
+      negara: "India",
+      kota: "Kolkota",
+      balance: 125000
+    },
+    {
+      name: 'Uday',
+      negara: "Indonesia",
+      kota: "Jakarta",
+      balance: 234000
+    },
+    {
+      name: 'Abhishek',
+      negara: "India",
+      kota: "Mumbai",
+      balance: 421000
+    },
+    {
+      name: 'Mayank',
+      negara: "Indonesia",
+      kota: "Jakarta",
+      balance: 104000
+    },
+    {
+      name: 'Ashish',
+      negara: "Pakistan",
+      kota: "Pakistan",
+      balance: 120000
+    },
+    {
+      name: 'Raju',
+      negara: "Indonesia",
+      kota: "Jakarta",
+      balance: 43000
+    },
+  ]
 }
