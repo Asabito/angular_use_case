d9a05c68b716d4d1c645e011508c33c7fcaaeccd
indrapurnayasa <indrapurnayasaa@gmail.com>
2023-12-19 16:37:21 +0700

    show data

---
+++ src/app/model/customer-model.ts
@@ -0,0 +1,10 @@
+export class customer{
+    createdAt?: string;
+    name?: string;
+    avatar?: string;
+    city?: string;
+    country?: string;
+    balance?: number;
+    currency?: string;
+    id?: string | number;
+}
